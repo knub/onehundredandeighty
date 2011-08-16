cb42ea319ac172ca9bcc9ecca28333fda01ee4f9
Stefan Bunk <stefan.bunk@googlemail.com>
2011-08-16 21:52:17 +0200

    Styled Drag'n'Drop elements.

--- src/main.js
+++ src/main.js
@@ -3,5 +3,4 @@ $(function() {
 		connectWith: ".semester-view",		// specifies lists where li's can be dropped
 		placeholder: "placeholder-highlight"	// css class for placeholder when drag'n dropping
 	}).disableSelection();				// disableSelection makes text selection impossible
-
 });
