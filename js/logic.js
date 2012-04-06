7f6071cf1fbdd24dfd2757b61aab39e4f759e368
Stefan Bunk <stefan.bunk@googlemail.com>
2012-04-06 23:41:15 +0200

    Add data for SS12.

--- js/logic.js
+++ js/logic.js
@@ -24,11 +24,11 @@ var semesterManager = {
 		*/
 	],
 	numberDisplayed: 6,
-	current: "WS11/12",
-	lastSummerSemester: "SS11",
+	current: "SS12",
+	lastSummerSemester: "SS12",
 	lastWinterSemester: "WS11/12",
-	startswith: "WS10/11",
 	/* the semester that is the first semester when you first start the application */
+	startswith: "WS10/11",
 	updateSemester: function(semester_number, semester_string) {
 		var index = semester_number - 1;
 		if (semester_string.search(/[WS]S((\d{2}\/\d{2})|(\d{2}))/) < 0) {
