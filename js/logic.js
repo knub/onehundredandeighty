e25d0630b2e9ac4f916e43dee352dc5f2d2d3cda
Stefan Bunk <stefan.bunk@googlemail.com>
2012-09-19 22:16:29 +0200

    Added date for WS12/13.

--- js/logic.js
+++ js/logic.js
@@ -24,9 +24,9 @@ var semesterManager = {
 		*/
 	],
 	numberDisplayed: 6,
-	current: "SS12",
+	current: "WS12/13",
 	lastSummerSemester: "SS12",
-	lastWinterSemester: "WS11/12",
+	lastWinterSemester: "WS12/13",
 	/* the semester that is the first semester when you first start the application */
 	startswith: "WS10/11",
 	updateSemester: function(semester_number, semester_string) {
