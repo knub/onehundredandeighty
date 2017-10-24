52c85aa067448d90ce83c6c9aa617d2a5170e2b5
Erik Brendel <erik.brendel@gmx.de>
2017-10-24 19:22:10 +0200

    hotfix

--- js/frontend.js
+++ js/frontend.js
@@ -625,7 +625,7 @@ const frontend = {
             return isNaN(gradeManager.get(course));
         }
         function needsGrade(course) {
-            return NEUE_STUDIENORDNUNG || getCourseParameter(course, 'modul').includes('Softskills')
+            return NEUE_STUDIENORDNUNG || !getCourseParameter(course, 'modul').includes('Softskills')
         }
         function courseToSelector(course) {
             if (course === 'bp')
