32e16e45348377b62d8eec7b4251dd9d7594a702
Erik Brendel <erik.brendel@gmx.de>
2018-02-11 23:24:06 +0100

    JQuery update + dragDrop nice css

--- js/pdfdrop.js
+++ js/pdfdrop.js
@@ -1,13 +1,26 @@
+
+
+let abortTimer;
 let dragTimer;
-$(document).dragover(function(e) {
-  const dt = e.originalEvent.dataTransfer;
-  if (dt.types && (dt.types.indexOf ? dt.types.indexOf('Files') !== -1 : dt.types.contains('Files'))) {
-    $("#dropHint").addClass('dropHintActive');
-    window.clearTimeout(dragTimer);
-  }
+
+$(document).on('dragover', function(e) {
+    const dt = e.originalEvent.dataTransfer;
+    if (dt.types && (dt.types.indexOf ? dt.types.indexOf('Files') !== -1 : dt.types.contains('Files'))) {
+        window.clearTimeout(dragTimer);
+        window.clearTimeout(abortTimer);
+        $("#dropHint").css('display', 'initial');
+        dragTimer = setTimeout(function() {
+            $("#dropHint").addClass('dropHintActive');
+        }, 50);
+    }
 });
-$(document).dragleave(function(e) {
-  dragTimer = window.setTimeout(function() {
-    $("#dropHint").removeClass('dropHintActive');
-  }, 25);
+$(document).on('dragleave', function(e) {
+    window.clearTimeout(dragTimer);
+    window.clearTimeout(abortTimer);
+    abortTimer = window.setTimeout(function() {
+        $("#dropHint").removeClass('dropHintActive');
+        setTimeout(function() {
+            $("#dropHint").css('display', 'none');
+        }, 500)
+    }, 25);
 });
