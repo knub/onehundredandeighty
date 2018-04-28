be0d8ba94a41242444c70ca509dcb95dd4c049a5
Erik Brendel <erik.brendel@gmx.de>
2018-04-28 16:10:40 +0200

    more tests

--- testing/test/test_data.js
+++ testing/test/test_data.js
@@ -5,4 +5,13 @@ describe('data.js', function() {
     it('has more than 50 LVs', () => {
         expect(Object.keys(data).length).to.be.greaterThan(50);
     });
+    describe('required LVs', () => {
+        const requiredLVs = ['gds', 'mathematik1','mod1', 'pt1', 'wirtschaft', 'mathematik2', 'mod2', 'mathematik2', 'recht1', 'bs', 'recht2', 'swa', 'ti1', 'swt1', 'ti2'];
+        for (let i = 0; i < requiredLVs.length; i++) {
+            const lv = requiredLVs[i];
+            it('contains ' + lv, () => {
+                expect(data[lv]).to.not.be.undefined;
+            });
+        }
+    });
 });
